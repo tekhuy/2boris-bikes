9ca5ed9b99ad2b2fb7f1e03e2c0c081e3402bac8
tekhuy <huyle20@hotmail.com>
2015-03-26 17:45:07 +0100

    to complete specify larger capacity when necessary

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,33 +1,32 @@
 class DockingStation
 
+  DEFAULT_CAPACITY = 20
+  
   def initialize
     @bikes = []
   end
 
   def dock bike
-    fail 'Station Full' if @bikes.length >= 20
+    fail 'Station Full' if full?
     @bikes << bike
     nil
   end
 
   def release_bike
-    fail 'No Bikes Available' if @bikes.empty?
+    fail 'No Bikes Available' if empty?
     @bikes.pop
   end
 
-  # require_relative 'bike'
-  #
-  # attr_writer :bike
-  # alias_method :dock, :bike=
-  #
-  # def release_bike
-  #   fail 'No Bikes Available' unless @bike
-  #   @bike
-  # end
-  #
-  # def dock bike
-  #   fail 'Station Full' if @bike
-  #   @bike = bike
-  # end
+  private
+
+  attr_reader :bikes
+
+  def full?
+    @bikes.length >= DEFAULT_CAPACITY
+  end
+
+  def empty?
+    @bikes.empty?
+  end
 
 end
